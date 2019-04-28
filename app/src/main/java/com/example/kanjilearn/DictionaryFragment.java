7fedb0837e8ad283650e9295730bc2e3870e6ef3
fleshka_1337 <figyshkin2@gmail.com>
2019-04-28 12:09:45 +1000

    Project
    CustomListView add

--- app/src/main/java/com/example/kanjilearn/DictionaryFragment.java
+++ app/src/main/java/com/example/kanjilearn/DictionaryFragment.java
@@ -14,15 +14,99 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
-//import android.app.Fragment;
-
-
+import android.widget.TextView;
 
 public class DictionaryFragment extends Fragment {
 
     private String value = "Hello!";
     private FragmentListener listener;
+
+    ListView listView;
+
+    String[] mTitle = {
+            "六",
+            "日",
+            "月"
+//            "百",
+//            "歳",
+//            "角",
+//            "大",
+//            "親",
+//            "第",
+//            "感",
+//            "本",
+//            "木",
+//            "分",
+//            "合",
+//            "週",
+//            "十",
+//            "万",
+//            "千",
+//            "曜",
+//            "水",
+//            "火",
+//            "数",
+//            "今",
+//            "昨",
+//            "明",
+//            "望",
+//            "先",
+//            "五",
+//            "来",
+//            "見",
+//            "夜",
+//            "円",
+//            "秒",
+//            "一",
+//            "二",
+//            "三",
+//            "四",
+//            "七",
+//            "八",
+//            "九",
+//            "入",
+//            "出",
+//            "半",
+//            "方",
+//            "外",
+//            "父",
+//            "母",
+//            "亡",
+//            "友",
+//            "切",
+//            "肉",
+//            "当",
+//            "人",
+//            "気",
+//            "語",
+//            "自",
+//            "国",
+//            "間",
+//            "字",
+//            "金",
+//            "土",
+//            "何",
+//            "生",
+//            "田",
+//            "力",
+//            "回",
+//            "肉"
+    };
+
+    String mDescriprion[] = {
+            "Kanji one - SIX",
+            "Kanji two - DAY",
+            "Kajni three - MOON"
+    };
+
+    int images[] = {
+        R.drawable.ic_six,
+        R.drawable.ic_day,
+        R.drawable.ic_moon
+    };
+
 
     public DictionaryFragment() {
         // Required empty public constructor
@@ -31,6 +115,10 @@ public class DictionaryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        listView = view.findViewById(R.id.dictionaryList);
+
+
     }
 
     @Override
@@ -53,15 +141,14 @@ public class DictionaryFragment extends Fragment {
 //            }
 //        });
 
-        ListView dictList = view.findViewById(R.id.dictionaryList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
-                android.R.layout.simple_list_item_1, getListOfWords());
-        dictList.setAdapter(adapter);
+        listView = view.findViewById(R.id.dictionaryList);
 
-        dictList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        MyAdapter adapter = new MyAdapter(getActivity(), mTitle, mDescriprion, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //listener.onItemClick(value);
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 FragmentManager manager = getFragmentManager();
                 FragmentTransaction transaction = manager.beginTransaction();
                 transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
@@ -69,80 +156,98 @@ public class DictionaryFragment extends Fragment {
                 transaction.commit();
             }
         });
+
+
+//        ListView dictList = view.findViewById(R.id.dictionaryList);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+//                android.R.layout.simple_list_item_1, getListOfWords());
+//        dictList.setAdapter(adapter);
+
+//        dictList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                //listener.onItemClick(value);
+//                FragmentManager manager = getFragmentManager();
+//                FragmentTransaction transaction = manager.beginTransaction();
+//                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//                transaction.replace(R.id.fragment_container, new DetailFragment()).addToBackStack(null);
+//                transaction.commit();
+//            }
+//        });
     }
 
-    String[] getListOfWords (){
-        String[] source = new String[]{
-                "六",
-                "日",
-                "月",
-                "百",
-                "歳",
-                "角",
-                "大",
-                "親",
-                "第",
-                "感",
-                "本",
-                "木",
-                "分",
-                "合",
-                "週",
-                "十",
-                "万",
-                "千",
-                "曜",
-                "水",
-                "火",
-                "数",
-                "今",
-                "昨",
-                "明",
-                "望",
-                "先",
-                "五",
-                "来",
-                "見",
-                "夜",
-                "円",
-                "秒",
-                "一",
-                "二",
-                "三",
-                "四",
-                "七",
-                "八",
-                "九",
-                "入",
-                "出",
-                "半",
-                "方",
-                "外",
-                "父",
-                "母",
-                "亡",
-                "友",
-                "切",
-                "肉",
-                "当",
-                "人",
-                "気",
-                "語",
-                "自",
-                "国",
-                "間",
-                "字",
-                "金",
-                "土",
-                "何",
-                "生",
-                "田",
-                "力",
-                "回",
-                "肉"
-        };
-        return source;
-    }
+//    String[] getListOfWords (){
+//        String[] source = new String[]{
+//                "六",
+//                "日",
+//                "月",
+//                "百",
+//                "歳",
+//                "角",
+//                "大",
+//                "親",
+//                "第",
+//                "感",
+//                "本",
+//                "木",
+//                "分",
+//                "合",
+//                "週",
+//                "十",
+//                "万",
+//                "千",
+//                "曜",
+//                "水",
+//                "火",
+//                "数",
+//                "今",
+//                "昨",
+//                "明",
+//                "望",
+//                "先",
+//                "五",
+//                "来",
+//                "見",
+//                "夜",
+//                "円",
+//                "秒",
+//                "一",
+//                "二",
+//                "三",
+//                "四",
+//                "七",
+//                "八",
+//                "九",
+//                "入",
+//                "出",
+//                "半",
+//                "方",
+//                "外",
+//                "父",
+//                "母",
+//                "亡",
+//                "友",
+//                "切",
+//                "肉",
+//                "当",
+//                "人",
+//                "気",
+//                "語",
+//                "自",
+//                "国",
+//                "間",
+//                "字",
+//                "金",
+//                "土",
+//                "何",
+//                "生",
+//                "田",
+//                "力",
+//                "回",
+//                "肉"
+//        };
+//        return source;
+//    }
 
     @Override
     public void onAttach(Context context) {
@@ -157,4 +262,37 @@ public class DictionaryFragment extends Fragment {
     public void setOnFragmentListener(FragmentListener listener){
         this.listener = listener;
     }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter (Context c,String title[], String description[],int imgs[]){
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row =layoutInflater.inflate(R.layout.row, parent,false);
+            ImageView images = row.findViewById(R.id.imageViewKanji);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+            //Установим наши ресурсы на view
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+            return row;
+        }
+    }
 }
+

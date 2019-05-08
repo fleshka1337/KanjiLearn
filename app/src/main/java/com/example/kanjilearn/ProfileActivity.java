e83cf9380a1071bcbaffd09ae0b8f99105fcb416
fleshka_1337 <figyshkin2@gmail.com>
2019-05-08 23:40:46 +1000

    Login and register
    Add simple registration and login

--- app/src/main/java/com/example/kanjilearn/ProfileActivity.java
+++ app/src/main/java/com/example/kanjilearn/ProfileActivity.java
@@ -1,23 +1,113 @@
 package com.example.kanjilearn;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.net.IDN;
+import java.util.Arrays;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 9119;
+    List<AuthUI.IdpConfig> providers;
+    Button sign_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        sign_out = (Button)findViewById(R.id.sign_out);
+
+        sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Logout
+                AuthUI.getInstance()
+                        .signOut(ProfileActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                sign_out.setEnabled(false);
+                                showSignInOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProfileActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Profile_activity");
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //init providers
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        showSignInOptions();
+    }
+
+    private void showSignInOptions(){
+        FirebaseApp.initializeApp(this);
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+              //  .setLogo(R.mipmap.ic_launcher_round)
+              //  .setLogo(R.drawable.test_background)
+                .setLogo(R.drawable.ic_launcher_web)
+                .setTheme(R.style.AuthUI_Theme)
+                .build(), MY_REQUEST_CODE
+        );
+        Toast.makeText(this,"Войдите или создайте аккаунт",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                // get User
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // Show Email on toast
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+                //set Button signout
+                sign_out.setEnabled(true);
+            }
+            else
+                {
+//                    Toast.makeText(this,""+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+                    overridePendingTransition(R.anim.fui_slide_in_right, R.anim.fui_slide_out_left);
+                }
+        }
     }
 
     @Override

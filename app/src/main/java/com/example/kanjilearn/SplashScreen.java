2966e76b65ffda0ecca9c6403dae2ea35d8e5a25
fleshka_1337 <figyshkin2@gmail.com>
2019-05-09 14:54:42 +1000

    Animation
    Add new animation

--- app/src/main/java/com/example/kanjilearn/SplashScreen.java
+++ app/src/main/java/com/example/kanjilearn/SplashScreen.java
@@ -9,16 +9,34 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
     public static int TIME_OUT = 1600;
+
+    private TextView textView;
+    private ProgressBar progressBar;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         //getSupportActionBar().hide();
+
+        textView = (TextView)findViewById(R.id.splash_Text);
+        progressBar = (ProgressBar)findViewById(R.id.splash_Progress);
+        imageView = (ImageView)findViewById(R.id.splash_Image);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.splash_transition);
+
+        textView.startAnimation(animation);
+        progressBar.startAnimation(animation);
+        imageView.startAnimation(animation);
 
         if (Build.VERSION.SDK_INT >= 21) {
             //Статус бар - вверху

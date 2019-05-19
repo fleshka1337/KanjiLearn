048dd2ffc5d22517172f84a45037bda4b14d38ec
fleshka_1337 <figyshkin2@gmail.com>
2019-05-19 18:16:27 +1000

    Firebase

--- app/src/main/java/com/example/kanjilearn/DictionaryFragment.java
+++ app/src/main/java/com/example/kanjilearn/DictionaryFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
--- app/src/main/java/com/example/kanjilearn/ProfileActivity.java
+++ app/src/main/java/com/example/kanjilearn/ProfileActivity.java
@@ -6,38 +6,66 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private static final int MY_REQUEST_CODE = 9119;
     List<AuthUI.IdpConfig> providers;
-    Button sign_out;
+    Button    sign_out
+            , test_send
+            , test_get;
+
+    EditText edit_data_FB;
+
+    TextView text_get;
+
+    String test;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        database = FirebaseDatabase.getInstance();
+//        myRef = database.getReference("item");
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         sign_out = (Button)findViewById(R.id.sign_out);
+        edit_data_FB = (EditText)findViewById(R.id.edit_data_FB);
+        text_get = (TextView)findViewById(R.id.text_get);
+        test_send = (Button)findViewById(R.id.test_send);
+        test_get = (Button)findViewById(R.id.test_get);
 
         //init providers
         providers = Arrays.asList(
@@ -81,6 +109,38 @@ public class ProfileActivity extends AppCompatActivity {
                 });
             }
         });
+
+
+        test_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+                String testData = edit_data_FB.getText().toString();
+
+                Map newPost = new HashMap();
+                newPost.put("testData",testData);
+
+                current_user_db.setValue(newPost);
+            }
+        });
+
+        test_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id).child("testData");
+                
+
+                //Map newPost = new HashMap();
+                //String data = newPost.get();
+
+                //text_get.setText();
+            }
+        });
+
+
     }
 
     private void showSignInOptions(){

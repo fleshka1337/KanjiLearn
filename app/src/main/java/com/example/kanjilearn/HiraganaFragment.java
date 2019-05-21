6a266a2f9b9c761f679e641dfd561eb1821f3da8
fleshka_1337 <figyshkin2@gmail.com>
2019-05-21 20:10:32 +1000

    cardView

--- app/src/main/java/com/example/kanjilearn/HiraganaFragment.java
+++ app/src/main/java/com/example/kanjilearn/HiraganaFragment.java
@@ -22,10 +22,10 @@ import java.util.Locale;
  */
 public class HiraganaFragment extends Fragment {
 
-    Button btnSpeak;
-    EditText edtSpeak;
-
-    TextToSpeech textToSpeech;
+//    Button btnSpeak;
+//    EditText edtSpeak;
+//
+//    TextToSpeech textToSpeech;
 
     public HiraganaFragment() {
 
@@ -37,55 +37,57 @@ public class HiraganaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_hiragana, container, false);
+//
+//        //Init text to speech
+//        textToSpeech = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
+//            @Override
+//            public void onInit(int status) {
+//
+//                if (status == TextToSpeech.SUCCESS){
+//                    int result = textToSpeech.setLanguage(Locale.JAPANESE);
+//                    if (result == TextToSpeech.LANG_MISSING_DATA ||
+//                            result == TextToSpeech.LANG_NOT_SUPPORTED){
+//                        Toast.makeText(getActivity(), "THis language is not supported", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        btnSpeak.setEnabled(true);
+////                        textToSpeech.setPitch(0.002f);
+////                        textToSpeech.setSpeechRate(2.0f);
+//                        textToSpeech.setSpeechRate(1.0f);
+//                        speak();
+//                    }
+//                }
+//            }
+//        });
+//
+//        //init View
+//        edtSpeak = (EditText)view.findViewById(R.id.hiragana_edit);
+//        btnSpeak = (Button)view.findViewById(R.id.hiragana_button);
+//        btnSpeak.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                speak();
+//            }
+//        });
 
-        //Init text to speech
-        textToSpeech = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
 
-                if (status == TextToSpeech.SUCCESS){
-                    int result = textToSpeech.setLanguage(Locale.JAPANESE);
-                    if (result == TextToSpeech.LANG_MISSING_DATA ||
-                            result == TextToSpeech.LANG_NOT_SUPPORTED){
-                        Toast.makeText(getActivity(), "THis language is not supported", Toast.LENGTH_SHORT).show();
-                    } else {
-                        btnSpeak.setEnabled(true);
-//                        textToSpeech.setPitch(0.002f);
-//                        textToSpeech.setSpeechRate(2.0f);
-                        textToSpeech.setSpeechRate(1.0f);
-                        speak();
-                    }
-                }
-            }
-        });
-
-        //init View
-        edtSpeak = (EditText)view.findViewById(R.id.hiragana_edit);
-        btnSpeak = (Button)view.findViewById(R.id.hiragana_button);
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                speak();
-            }
-        });
 
         return view;
     }
 
-    private void speak(){
-        String text = edtSpeak.getText().toString();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null,null);
-        else
-            textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null);
-    }
+//    private void speak(){
+//        String text = edtSpeak.getText().toString();
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+//            textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null,null);
+//        else
+//            textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+//    }
 
     @Override
     public void onDestroy() {
-        if (textToSpeech != null){
-            textToSpeech.stop();
-            textToSpeech.shutdown();
-        }
+//        if (textToSpeech != null){
+//            textToSpeech.stop();
+//            textToSpeech.shutdown();
+//        }
         super.onDestroy();
     }
 }
